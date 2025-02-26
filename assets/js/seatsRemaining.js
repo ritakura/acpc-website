72555a0d9805288542065f24ecc4d6754c2ba3ed
Jason Feng <34261800+JasonFeng365@users.noreply.github.com>
2025-02-25 23:39:41 -0800

    Inb4 Oracle charges me $20 for hosting CANIS, can Lego give me a discount? (#12)
    * CANIS signups endpoint integration. I am a god????
    
    * Add Audrey's picture. Still waiting on three!

---
+++ assets/js/seatsRemaining.js
@@ -0,0 +1,43 @@
+let vue = new Vue({
+	el: '#seatsRemaining',
+	data: {
+		endpoint:"https://canis.jasonfeng365.top/api/contests/53807620/signups/",
+
+		registered:0,
+		onePersonTeams:0,
+		twoPersonTeams:0,
+
+		capacity:200,
+
+		seatsRemaining:"",
+	},
+	methods: {
+		sendRequest() {
+			fetch(this.endpoint, {method: "GET"})
+			.then(success => {
+				success.json().then(json => {
+					this.registered = json.responseCount
+					console.log(json)
+
+					this.onePersonTeams = json.teamSizeMap["No"]
+					this.twoPersonTeams = Math.floor(json.teamSizeMap["Yes"] / 2)
+
+					this.setText()
+				})
+			})
+
+		},
+		setText() {
+			this.seatsRemaining = Math.max(0, this.capacity - this.registered)
+			this.seatsRemaining += " spots left!"
+			console.log(this.registered)
+			console.log(this.onePersonTeams)
+			console.log(this.twoPersonTeams)
+
+
+		}
+	},
+	mounted: function() {
+		this.sendRequest()
+	}
+});
