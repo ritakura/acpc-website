25ff247d3f53b2be5837f1a0fe9cf58ba5d26170
JasonFeng365 <34261800+JasonFeng365@users.noreply.github.com>
2025-02-26 02:18:24 -0800

    Use ngrok reverse proxy for now

--- assets/js/seatsRemaining.js
+++ assets/js/seatsRemaining.js
@@ -1,7 +1,7 @@
 let vue = new Vue({
 	el: '#seatsRemaining',
 	data: {
-		endpoint:"https://canis.jasonfeng365.top/api/contests/53807620/signups/",
+		endpoint:"https://immortal-hot-cat.ngrok-free.app/api/contests/53807620/signups/",
 
 		registered:0,
 		onePersonTeams:0,
@@ -13,7 +13,12 @@ let vue = new Vue({
 	},
 	methods: {
 		sendRequest() {
-			fetch(this.endpoint, {method: "GET"})
+			fetch(this.endpoint, {
+				method: "GET",
+				headers: new Headers({
+					"ngrok-skip-browser-warning": true
+				})
+			})
 			.then(success => {
 				success.json().then(json => {
 					this.registered = json.responseCount
